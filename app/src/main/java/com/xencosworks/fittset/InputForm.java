be899cd1a2ad9084dfac4105af37cfe445735c50
bola <bola_fah.97@hotmail.com>
2019-04-16 16:58:21 +0200

    revamped input form, now it is bug free and totally works as a new input or to edit an exsisting exercise

--- app/src/main/java/com/xencosworks/fittset/InputForm.java
+++ app/src/main/java/com/xencosworks/fittset/InputForm.java
@@ -14,8 +14,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shawnlin.numberpicker.NumberPicker;
 
@@ -31,34 +31,27 @@ public class InputForm extends AppCompatActivity {
     private ExerciseViewModel exerciseViewModel;
     private Exercise exercise = null;
 
-    private View AlteringExerciseContents;
-    private ImageView AlteringExerciseArrow;
+    private final String[] musclesArray = {"Unspecified", "Chest", "Shoulders", "Back", "Biceps", "Triceps", "Legs", "Abs"};
 
-    private EditText exTitleIP;
-    private EditText exNotesIP;
-    private TextView musclesTv;
+
+    // Take input from here
+    private EditText exerciseNameEt;
+    private EditText exerciseNotesEt;
+
+    // Show input here
+    private TextView musclesGroupTv;
     private TextView maxWeightTv;
     private TextView setsRepsTv;
 
+    // Fields to be added/edited to a new exercise object
+    private String exerciseName;
+    private String exerciseNotes;
+    private int muscleGroup = 0;
+    private int maxWeight = 0;
+    private int exerciseReps = 10;
+    private int exerciseSets = 4;
 
-    private int mExerciseId = -1;
-    private String mExerciseName = "";
-    private String mExerciseNotes = "";
-
-    private int tempMuscleGroup = 0;
-    private int mMuscleGroup=0;
-
-    private int tempMaxWeight = 1;
-    private int mMaxWeight=0;
-
-    private int tempSets = 4;
-    private int mSets=0;
-
-    private int tempReps = 10;
-    private int mReps=0;
-
-    private final String[] musclesArr = {"Unspecified", "Chest", "Shoulders", "Back", "Biceps", "Triceps", "Legs", "Abs"};
-
+    // IDS to help in routing from different activities.
     public static String EXTRA_ID = "com.xencosworks.fittset.InputForm.EXTRA_ID";
     public static String EXTRA_NAME = "com.xencosworks.fittset.InputForm.EXTRA_NAME";
     public static String EXTRA_NOTES = "com.xencosworks.fittset.InputForm.EXTRA_NOTES";
@@ -67,10 +60,13 @@ public class InputForm extends AppCompatActivity {
     public static String EXTRA_SETS = "com.xencosworks.fittset.InputForm.EXTRA_SETS";
     public static String EXTRA_REPS = "com.xencosworks.fittset.InputForm.EXTRA_REPS";
 
+    // This activity can be launched via three states.
     public static final int ADD_EXERCISE_REQUEST = 1;
     public static final int EDIT_EXERCISE_REQUEST = 2;
+    public static final int EMPTY_DAY_REQUEST = 3;
 
-    private int currentState = 0;
+    private int currentState = ADD_EXERCISE_REQUEST;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,85 +78,35 @@ public class InputForm extends AppCompatActivity {
         initSetup();
         setUpFab();
         underlineEffect();
-        initializeViews();
+        currentState = ADD_EXERCISE_REQUEST; //RESET TO DEFAULT STATE
+        intent = null;                       //RESET any previous intents
+        intent = getIntent();
 
-        Intent intent = getIntent();
-
+        // Call this function to preEdit fields for the user.
         if(intent.hasExtra(EXTRA_ID)){
-            customCodeHelper(intent, EDIT_EXERCISE_REQUEST);
-        }else if(intent.hasExtra(EXTRA_NAME)) {
-            customCodeHelper(intent, ADD_EXERCISE_REQUEST);
+            currentState = EDIT_EXERCISE_REQUEST;
+            setUpFab(); // run again to change fab icon
+            customCodeHelper(intent, currentState);
+        }else if(intent.hasExtra(EXTRA_MUSCLE_GROUP)) {
+            currentState = EMPTY_DAY_REQUEST;
+            customCodeHelper(intent, currentState);
         }
-    }
-
-    private void initializeViews(){
-        exTitleIP = findViewById(R.id.input_form_ex_title);
-        exNotesIP = findViewById(R.id.input_form_ex_notes);
-        musclesTv = findViewById(R.id.input_form_ex_muscle_gp_tv);
-        maxWeightTv = findViewById(R.id.input_form_ex_max_weight_tv);
-        setsRepsTv = findViewById(R.id.input_form_ex_sets_reps_tv);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                //NavUtils.navigateUpFromSameTask(this);
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void customCodeHelper(Intent intent, int requestCode){
-        switch (requestCode){
-            case ADD_EXERCISE_REQUEST:
-                mMuscleGroup = intent.getIntExtra(EXTRA_MUSCLE_GROUP, -1);
-                musclesTv.setText(musclesArr[mMuscleGroup]);
-
-                currentState = ADD_EXERCISE_REQUEST;
-                break;
-            case EDIT_EXERCISE_REQUEST:
-                mExerciseId = intent.getIntExtra(EXTRA_ID, -1);
-
-                mExerciseName = intent.getStringExtra(EXTRA_NAME);
-                exTitleIP.setText(mExerciseName);
-
-                mExerciseNotes = intent.getStringExtra(EXTRA_NOTES);
-                exNotesIP.setText(mExerciseNotes);
-
-                mMuscleGroup = intent.getIntExtra(EXTRA_MUSCLE_GROUP, -1);
-                musclesTv.setText(musclesArr[mMuscleGroup]);
-
-                mReps = intent.getIntExtra(EXTRA_REPS, -1);
-                mSets = intent.getIntExtra(EXTRA_SETS, -1);
-                setsRepsTv.setText(getString(R.string.sets_reps, mSets, mReps));
-
-                mMaxWeight = intent.getIntExtra(EXTRA_MAX_WEIGHT, -1);
-                maxWeightTv.setText(getString(R.string.kg, mMaxWeight));
-
-                currentState = EDIT_EXERCISE_REQUEST;
-                break;
-            default:
-                return;
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_input_form, menu);
-        return true;
     }
 
     private void initSetup(){
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_input_usage);
+        Toolbar mToolbar = findViewById(R.id.toolbar_input_usage);
         setSupportActionBar(mToolbar);
 
         //For the Back Button, important because you set a custom toolbar
         ActionBar actionBar = this.getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //-----------------------------------------------------------------
+        exerciseNameEt = findViewById(R.id.input_form_ex_title);
+        exerciseNotesEt = findViewById(R.id.input_form_ex_notes);
+        musclesGroupTv = findViewById(R.id.input_form_ex_muscle_gp_tv);
+        maxWeightTv = findViewById(R.id.input_form_ex_max_weight_tv);
+        setsRepsTv = findViewById(R.id.input_form_ex_sets_reps_tv);
     }
 
     private void setUpFab(){
@@ -168,32 +114,84 @@ public class InputForm extends AppCompatActivity {
         actionButton.setButtonColor(getResources().getColor(R.color.colorAccent));
         actionButton.setButtonColorPressed(getResources().getColor(R.color.colorAccentDark));
         actionButton.setImageResource(R.drawable.fab_plus_icon);
+        if (currentState == EDIT_EXERCISE_REQUEST){
+            actionButton.setImageResource(R.drawable.ic_edit_white_24dp);
+        }
 
         actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 pullData(currentState);
-                finish();
             }
         });
     }
 
     private void pullData(int requestCode){
-        if(requestCode == ADD_EXERCISE_REQUEST){
-            String exTitle = exTitleIP.getText().toString();
-            String exNotes = exNotesIP.getText().toString();
+        //Fetch unUpdated data first
+        exerciseName = exerciseNameEt.getText().toString();
+        exerciseNotes = exerciseNotesEt.getText().toString();
+        //Other fields are already updated  via @showDialog() functions
 
-            exercise = new Exercise(exTitle, exNotes, mMuscleGroup, mMaxWeight, mMaxWeight, mSets, mReps, 0);
-            exerciseViewModel.insert(exercise);
-            Log.e(LOGTAG, "--------------------------OTHER WAY");
-        }else if(requestCode == EDIT_EXERCISE_REQUEST){
-            exercise = new Exercise(mExerciseName, mExerciseNotes, mMuscleGroup, mMaxWeight, mMaxWeight, mSets, mReps, 0);
-            exercise.setId(mExerciseId);
-            exerciseViewModel.update(exercise);
-            Log.e(LOGTAG, "--------------------------TRIED TO UPDATE");
+        switch (requestCode){
+            // Both cases needs adding a new exercise
+            case EMPTY_DAY_REQUEST:
+            case ADD_EXERCISE_REQUEST:
+                if(exerciseName.length()!=0 && maxWeight!=0 && muscleGroup!=0){
+                    exerciseViewModel.insert(new Exercise(exerciseName, exerciseNotes,
+                            muscleGroup, maxWeight, maxWeight, exerciseSets, exerciseReps, 0));
+                    Toast.makeText(this, "New Exercise Added", Toast.LENGTH_LONG).show();
+                    finish();
+                }else {
+                    Toast.makeText(this, "Make sure to update required fields first", Toast.LENGTH_LONG).show();
+                }
+
+                break;
+
+            // But this case needs editing an existing exercise
+            case EDIT_EXERCISE_REQUEST:
+                if(exerciseName.length()!=0 && maxWeight!=0 && muscleGroup!=0){
+                    Exercise exercise = new Exercise(exerciseName, exerciseNotes,
+                            muscleGroup, maxWeight, maxWeight, exerciseSets, exerciseReps, 0);
+                    exercise.setId(intent.getIntExtra(EXTRA_ID, -1));
+                    exerciseViewModel.update(exercise);
+                    Toast.makeText(this, "Exercise Updated", Toast.LENGTH_LONG).show();
+                    finish();
+                }else {
+                    Toast.makeText(this, "Make sure to update required fields first", Toast.LENGTH_LONG).show();
+                }
         }
-
     }
+
+    private void customCodeHelper(Intent intent, int requestCode){
+        switch (requestCode){
+            case EMPTY_DAY_REQUEST:
+                muscleGroup = intent.getIntExtra(EXTRA_MUSCLE_GROUP, 0);
+                refreshUi();
+                break;
+            case EDIT_EXERCISE_REQUEST:
+                exerciseName = intent.getStringExtra(EXTRA_NAME);
+                exerciseNotes = intent.getStringExtra(EXTRA_NOTES);
+                muscleGroup = intent.getIntExtra(EXTRA_MUSCLE_GROUP, 0);
+                maxWeight = intent.getIntExtra(EXTRA_MAX_WEIGHT, 0);
+                exerciseSets = intent.getIntExtra(EXTRA_SETS, 0);
+                exerciseReps = intent.getIntExtra(EXTRA_REPS, 0);
+                refreshUi();
+        }
+    }
+
+    private void refreshUi() {
+        exerciseNameEt.setText(exerciseName);
+        exerciseNotesEt.setText(exerciseNotes);
+        musclesGroupTv.setText(musclesArray[muscleGroup]);
+        maxWeightTv.setText(getString(R.string.kg, maxWeight));
+        setsRepsTv.setText(getString(R.string.sets_reps, exerciseSets, exerciseReps));
+    }
+
+    /**
+     * @underlineEffect() handles drawing an altering effect when user taps different textboxes
+     * and then calls the @showDialog() functions which in turn pull data which user inputed and
+     * updates the main InputForm.java class fields initialized with default values on top of
+     * the document.*/
 
     private void underlineEffect(){
         final View exGroupIP = findViewById(R.id.input_form_ex_muscle_gp);
@@ -220,7 +218,6 @@ public class InputForm extends AppCompatActivity {
             }
         });
 
-
         exSets_RepsIP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -230,25 +227,6 @@ public class InputForm extends AppCompatActivity {
                 showDialogSetsReps();
             }
         });
-
-        View alteringExercise = findViewById(R.id.input_form_custom_routine);
-        AlteringExerciseContents = findViewById(R.id.input_form_altering_exercise_contents);
-        AlteringExerciseArrow = findViewById(R.id.input_form_custom_routine_arrow);
-
-        alteringExercise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (AlteringExerciseContents.getVisibility()==View.VISIBLE){
-                    AlteringExerciseContents.setVisibility(View.GONE);
-                    AlteringExerciseArrow.setImageResource(R.drawable.ic_input_expand);
-                }else {
-                    AlteringExerciseContents.setVisibility(View.VISIBLE);
-                    AlteringExerciseArrow.setImageResource(R.drawable.ic_input_collapse);
-                }
-            }
-        });
-
-
     }
 
     private void showDialogMuscles(){
@@ -264,26 +242,25 @@ public class InputForm extends AppCompatActivity {
 
 
         numberPickerMuscleG.setMinValue(0);
-        numberPickerMuscleG.setMaxValue(musclesArr.length-1);
-        numberPickerMuscleG.setDisplayedValues(musclesArr);
-        numberPickerMuscleG.setValue(tempMuscleGroup);
+        numberPickerMuscleG.setMaxValue(musclesArray.length-1);
+        numberPickerMuscleG.setDisplayedValues(musclesArray);
+        numberPickerMuscleG.setValue(muscleGroup);
 
         numberPickerMuscleG.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                tempMuscleGroup = newVal;
+                muscleGroup = newVal;
             }
         });
 
-        musclesTv = findViewById(R.id.input_form_ex_muscle_gp_tv);
+        musclesGroupTv = findViewById(R.id.input_form_ex_muscle_gp_tv);
 
         Button confirmButton = dialog.findViewById(R.id.input_form_dialog_choose_muscle);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.cancel();
-                mMuscleGroup = tempMuscleGroup;
-                musclesTv.setText(musclesArr[mMuscleGroup]);
+                musclesGroupTv.setText(musclesArray[muscleGroup]);
             }
         });
     }
@@ -301,19 +278,18 @@ public class InputForm extends AppCompatActivity {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 //Log.e("WEIGHT", String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal));
-                tempMaxWeight = newVal;
+                maxWeight = newVal;
             }
         });
 
-        numberPickerMaxWeight.setValue(tempMaxWeight);
+        numberPickerMaxWeight.setValue(maxWeight);
 
         Button confirmButton = dialog.findViewById(R.id.input_form_dialog_choose_weight);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.cancel();
-                mMaxWeight = tempMaxWeight;
-                maxWeightTv.setText(getString(R.string.kg, mMaxWeight));
+                maxWeightTv.setText(getString(R.string.kg, maxWeight));
             }
         });
 
@@ -328,15 +304,15 @@ public class InputForm extends AppCompatActivity {
         dialog.show();
 
         NumberPicker numberPickerSets = dialog.findViewById(R.id.number_picker_sets);
-        numberPickerSets.setValue(tempSets);
+        numberPickerSets.setValue(exerciseSets);
 
         NumberPicker numberPickerReps = dialog.findViewById(R.id.number_picker_reps);
-        numberPickerReps.setValue(tempReps);
+        numberPickerReps.setValue(exerciseReps);
 
         numberPickerReps.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                tempReps = newVal;
+                exerciseReps = newVal;
             }
         });
 
@@ -344,7 +320,7 @@ public class InputForm extends AppCompatActivity {
         numberPickerSets.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                tempSets = newVal;
+                exerciseSets = newVal;
             }
         });
 
@@ -353,19 +329,33 @@ public class InputForm extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dialog.cancel();
-                mReps = tempReps;
-                mSets = tempSets;
-                setsRepsTv.setText(getString(R.string.sets_reps, mSets, mReps));
+                setsRepsTv.setText(getString(R.string.sets_reps, exerciseSets, exerciseReps));
             }
         });
     }
 
-    //TODO: Avoid Recreating new variables in each dialog, and try to reduce memory usage - later update
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_input_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                //NavUtils.navigateUpFromSameTask(this);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     //TODO: Add card to view unknown groups or
                                 //TODO: add prompt to disable adding unknown group
     //TODO: Add prompt to change unchanged fields and to save changes
-
-    //TODO: BUG FIX: being redirected from the all exercises, the muscle group is already chosen
-    //TODO:          but user must rechoose it in order to add the exercise successfully
+    //TODO: Altering Exercise option.
 }
